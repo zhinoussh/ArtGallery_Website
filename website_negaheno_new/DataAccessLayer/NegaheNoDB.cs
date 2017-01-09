f76937c4f9146b97ed0f774991379a7db47bcf07
zhinoussh <spring1986@gmail.com>
2017-01-09 10:03:36 +0330

    add db context

---
+++ website_negaheno_new/DataAccessLayer/NegaheNoDB.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using website_negaheno.Models;
+
+namespace website_negaheno.DataAccessLayer
+{
+    public class NegaheNoDB: DbContext
+    {
+        public virtual DbSet<tbl_art_gallery> tbl_art_gallery { get; set; }
+
+        public NegaheNoDB()
+            : base("name=NegaheNoConnection")
+        {
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
